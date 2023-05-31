155c3d6d2a2cd7426dec9578d1c2e05d63118c1e
Ancellist <83424461+Ancellist@users.noreply.github.com>
2023-05-31 08:12:16 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> testList = new ArrayList<>();
-        ArrayList<Integer> testList1 = new ArrayList<>();
+        LinkedList<Integer> testList = new LinkedList<>();
         for (int i = 0;i< 10;i++){
             Scanner scanner = new Scanner(System.in);
             int num = scanner.nextInt();
